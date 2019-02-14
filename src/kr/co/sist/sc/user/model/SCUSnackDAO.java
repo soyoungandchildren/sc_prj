a2e3fb2604354f95a729df303f725ef75d8469a2
soyoung1016 <loosa33@naver.com>
2019-02-14 18:38:57 +0900

    스낵뷰 업데이트
    판매중인 스낵 select

---
+++ src/kr/co/sist/sc/user/model/SCUSnackDAO.java
@@ -0,0 +1,66 @@
+package kr.co.sist.sc.user.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.sist.sc.user.vo.SCUSearchMenuVO;
+
+public class SCUSnackDAO {
+	private static SCUSnackDAO ssDAO;
+	
+	public SCUSnackDAO() {
+	}//SCUSnackDAO
+	
+	public static SCUSnackDAO getInstance() {
+		if(ssDAO == null) {
+			ssDAO = new SCUSnackDAO();
+		}//end if
+		
+		return ssDAO;
+	}//getInstance
+	
+	public List<SCUSearchMenuVO> selectSnackMenu() throws SQLException {
+		List<SCUSearchMenuVO> list = new ArrayList<>();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			//드라이버 로딩, connection
+			con = SCUConnect.getInstance().getConnection();
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("select snack_img, snack_name from snack where check_delete='N'");
+			
+			pstmt = con.prepareStatement(sql.toString());
+			
+			rs = pstmt.executeQuery();
+			
+			SCUSearchMenuVO ssmv = null;
+			while(rs.next()) {
+				ssmv = new SCUSearchMenuVO(rs.getString("snack_img"), rs.getString("snack_name"));
+				list.add(ssmv);
+			}//end while
+		
+		//연결 끊기
+		} finally {
+			if(rs != null) {
+				rs.close();
+			}//end if
+			if(pstmt != null) {
+				pstmt.close();
+			}//end if
+			if(con != null) {
+				con.close();
+			}//end if
+		}//end finally
+		
+		return list;
+	}//selectSnackMenu
+	
+}//class
---
+++ src/kr/co/sist/sc/user/vo/SCUSearchMenuVO.java
@@ -0,0 +1,25 @@
+package kr.co.sist.sc.user.vo;
+
+public class SCUSearchMenuVO {
+	//판매중인 스낵 불러오기
+	private String snack_img, snack_name;
+	
+	public SCUSearchMenuVO(String snack_img, String snack_name) {
+		this.snack_img = snack_img;
+		this.snack_name = snack_name;
+	}
+
+	public String getSnack_img() {
+		return snack_img;
+	}
+
+	public String getSnack_name() {
+		return snack_name;
+	}
+
+	@Override
+	public String toString() {
+		return "SCUSearchMenuVO [snack_img=" + snack_img + ", snack_name=" + snack_name + "]";
+	}
+	
+}
