2a813b015844345787636aa2f168a351cdc7bbfe
AZALEA <whatismyname@naver.com>
2019-02-22 17:54:46 +0900

    190222
    마이페이지 비밀번호 검증,  뷰 부분적으로 작성

--- src/kr/co/sist/sc/user/controller/SCUMainController.java
+++ src/kr/co/sist/sc/user/controller/SCUMainController.java
@@ -4,15 +4,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
+import kr.co.sist.sc.user.model.SCUMainDAO;
 import kr.co.sist.sc.user.view.SCULoginView;
 import kr.co.sist.sc.user.view.SCUMainView;
 import kr.co.sist.sc.user.view.SCUMovieListView;
+import kr.co.sist.sc.user.view.SCUMyPageView;
 import kr.co.sist.sc.user.view.SCUSnackMenuView;
+import kr.co.sist.sc.user.vo.SCULoginVO;
 
 public class SCUMainController extends WindowAdapter implements ActionListener{
 	
 	private SCUMainView smv;
+	private int cntCheckPassword;
 	
 	public SCUMainController(SCUMainView smv) {
 		this.smv = smv;
@@ -29,6 +37,38 @@ public class SCUMainController extends WindowAdapter implements ActionListener{
 	}//setMain Method
 	
 	
+	public void checkPassword() {
+		if(cntCheckPassword==5) {
+			JOptionPane.showMessageDialog(smv, "나중에 다시 시도해주세요");
+			cntCheckPassword=0;
+			return;
+		}//end if
+		
+		JPasswordField jpf = new JPasswordField();
+		int commit = JOptionPane.showConfirmDialog(smv, jpf, "패스워드 입력", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		if(commit==0) {
+			String password = new String(jpf.getPassword());
+			
+			boolean checkPassword;
+			try {
+				SCULoginVO slVO = new SCULoginVO(smv.getIdConnecting(), password);
+				checkPassword = SCUMainDAO.getInstance().checkPassword(slVO);
+				
+				if(checkPassword) {
+					cntCheckPassword = 0;
+					new SCUMyPageView(smv);
+				}else {
+					JOptionPane.showMessageDialog(smv, "비밀번호를 확인해주세요.");
+					cntCheckPassword+=1;
+					checkPassword();
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}//try~catch
+			
+		}//end if
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent ae) {
@@ -49,6 +89,14 @@ public class SCUMainController extends WindowAdapter implements ActionListener{
 		
 		if(ae.getSource().equals(smv.getJbtnSnack())) {//스낵 버튼
 			new SCUSnackMenuView(smv);
+		}//end if
+		
+		if(ae.getSource().equals(smv.getJbtnMyPage())) {
+			if(smv.getIsLogin()) {
+				checkPassword();
+			}else {
+				JOptionPane.showMessageDialog(smv, "로그인을 해주세요.");
+			}
 		}//end if
 		
 	}//actionPerformed
--- src/kr/co/sist/sc/user/model/SCUMainDAO.java
+++ src/kr/co/sist/sc/user/model/SCUMainDAO.java
@@ -7,11 +7,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import kr.co.sist.sc.user.vo.SCULoginVO;
 import kr.co.sist.sc.user.vo.SCUMainVO;
 
 public class SCUMainDAO {
 	
 	private static SCUMainDAO smDAO;
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 	
 	private SCUMainDAO() {
 	}//Constructor
@@ -28,9 +32,9 @@ public class SCUMainDAO {
 	public List<SCUMainVO> setMain() throws SQLException{
 		List<SCUMainVO> list = new ArrayList<>();
 		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		con = null;
+		pstmt = null;
+		rs = null;
 		
 		try {
 			con = SCUConnect.getInstance().getConnection();
@@ -40,12 +44,56 @@ public class SCUMainDAO {
 			
 			
 		}finally {
-			if(rs!=null) {rs.close();}
-			if(pstmt!=null) {pstmt.close();}
-			if(con!=null){con.close();}
+			disconnect();
 		}//end finally
 		
 		return list;
 	}//setMain Method
+	
+	public boolean checkPassword(SCULoginVO slVO) throws SQLException{
+		boolean flag = false;
+		
+		con = null;
+		pstmt =null;
+		rs = null;
+		
+		try {
+			String idConnecting = slVO.getMember_id();
+			String inputPassword = slVO.getPassword();
+			con = SCUConnect.getInstance().getConnection();
+			StringBuilder sqlSelectPassword = new StringBuilder();
+			sqlSelectPassword
+			.append("select password ")
+			.append("from member ")
+			.append("where member_id = ? ");
+			
+			pstmt = con.prepareStatement(sqlSelectPassword.toString());
+			pstmt.setString(1, idConnecting);
+			System.out.println("여기 오긴하냐");
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				System.out.println(inputPassword);
+				System.out.println(rs.getString("password"));
+				
+				if(rs.getString("password").equals(inputPassword)) {
+					flag = true;
+				}//end if
+				
+			}//end if
+			
+		}finally {
+			disconnect();
+		}
+		
+		return flag;
+	}//checkPassword
+	
+	private void disconnect() throws SQLException{
+		if(rs!=null) {rs.close();}
+		if(pstmt!=null) {pstmt.close();}
+		if(con!=null) {con.close();}
+	}
+	
 
 }//Class
