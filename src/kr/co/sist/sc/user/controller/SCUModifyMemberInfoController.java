3a6190a173d852d77a63fc95acdd7bde5fa2740a
AZALEA <whatismyname@naver.com>
2019-03-11 20:39:39 +0900

    asdf
    asdf

--- src/kr/co/sist/sc/user/controller/SCUModifyMemberInfoController.java
+++ src/kr/co/sist/sc/user/controller/SCUModifyMemberInfoController.java
@@ -3,9 +3,7 @@ package kr.co.sist.sc.user.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
-import java.rmi.server.UnicastRemoteObject;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
@@ -78,32 +76,35 @@ public class SCUModifyMemberInfoController extends WindowAdapter implements Acti
 			
 			
 			if("".equals(iName.replaceAll(" ", ""))) {
-				msgCenter("이름은 공백일 수 없습니다.");
-				smmiv.getJtfName().requestFocus();
-				smmiv.getJtfName().setText("");
+				initializeNameField();
 				return;
 			}else if("".equals((iPhoneCombine.toString().replaceAll("-", "")).replaceAll(" ", ""))){
-				msgCenter("전화번호는 공백일 수 없습니다.");
 				initializeNumberField();
 				return;
 			}else if(2>iName.length()||10<iName.length()) {
-				msgCenter("이름은 2자에서 10자 사이입니다.");
-				smmiv.getJtfName().setText("");
-				smmiv.getJtfName().requestFocus();
+				initializeNameField();
 				return;
 			}else if(frontPhone.length()!=3||middlePhone.length()>4||middlePhone.length()<3||
 					lastPhone.length()<3||lastPhone.length()>4) {
-				msgCenter("휴대전화 연락처 형식을 확인해주세요.");
 				initializeNumberField();
 				return;
 			}
+			
+//			for(int i = 0; i<iName.length(); i++) {
+//				if((44032<iName.codePointAt(i)&&iName.codePointAt(i)<55203)||
+//						(97<iName.codePointAt(i)&&iName.codePointAt(i)<122)/*||
+//						65<iName.codePointAt(i)&&iName.codePointAt(i)<90*/) {//영대소문자, 소리마디가 아니라면
+//				}else {
+//					initializeNameField();
+//					return;
+//				}
+//			}//end for
 			
 			try {
 				new Integer(frontPhone);
 				new Integer(middlePhone);
 				new Integer(lastPhone);
 			}catch(NumberFormatException nfe) {
-				msgCenter("휴대전화 연락처 형식을 확인해주세요.");
 				initializeNumberField();
 				return;
 			}
@@ -119,10 +120,16 @@ public class SCUModifyMemberInfoController extends WindowAdapter implements Acti
 	}
 	
 	private void initializeNumberField() {
+		msgCenter("휴대전화 양식을 확인해주세요.");
 		smmiv.getJtfPhone1().setText("");
 		smmiv.getJtfPhone2().setText("");
 		smmiv.getJtfPhone3().setText("");
 		smmiv.getJtfPhone1().requestFocus();
+	}
+	private void initializeNameField() {
+		msgCenter("작성하신 이름을 확인해주세요.");
+		smmiv.getJtfName().setText("");
+		smmiv.getJtfName().requestFocus();
 	}
 
 	
